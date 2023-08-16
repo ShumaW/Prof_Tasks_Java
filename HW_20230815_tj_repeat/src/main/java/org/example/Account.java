bc6b077c56701142e32beffa16dc55d3329c13c1
Yevgeniy Shulimenko <evgeniy.shulimenko@gmail.com>
2023-08-16 10:31:49 +0200

    HW 15.08.2023

--- HW_20230815_tj_repeat/src/main/java/org/example/Account.java
+++ HW_20230815_tj_repeat/src/main/java/org/example/Account.java
@@ -2,6 +2,8 @@ package org.example;
 
 import lombok.*;
 
+import java.util.Random;
+
 @Getter
 @Setter
 public class Account {
@@ -15,14 +17,14 @@ public class Account {
         if (balance < 0){
             throw new IllegalArgumentException();
         }
-        this.id = "000000";
+        this.id = generateId(6);
         this.status = status;
         this.zone = zone;
         this.balance = balance;
     }
 
     public Account() {
-        this.id = "000000";
+        this.id = generateId(6);
         this.zone = Zone.ZONE_1;
         this.balance = 0.00;
     }
@@ -35,5 +37,16 @@ public class Account {
         ZONE_1,
         ZONE_2,
         ZONE_3
+    }
+
+    private static String generateId(int length) {
+        int leftLimit = 97;
+        int rightLimit = 122;
+        Random random = new Random();
+
+        return random.ints(leftLimit, rightLimit + 1)
+                .limit(length)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
     }
 }
